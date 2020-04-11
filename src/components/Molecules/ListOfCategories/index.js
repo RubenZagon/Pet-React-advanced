917585f97fd705f04f79b11dee9f71923e8d9d78
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-11 16:48:25 +0100

    Implement FETCH on ListOfCategories

--- src/components/Molecules/ListOfCategories/index.js
+++ src/components/Molecules/ListOfCategories/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Category} from "../../Atoms/Category";
 import {Item, List} from "./styles";
-import {categories} from '../../../../api/db'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(function () {
+    fetch('https://petgram-server-ruben.rubenzagon.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, []);
+
   return (
     <List>
       {
