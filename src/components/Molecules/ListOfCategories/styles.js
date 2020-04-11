ec54a51e26ef3a9eed31a7de92f70ed5549c5170
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-11 12:04:51 +0100

    Used real information in categories

--- src/components/Molecules/ListOfCategories/styles.js
+++ src/components/Molecules/ListOfCategories/styles.js
@@ -4,9 +4,13 @@ export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-`
+  // Ocultar el scroll pero mantener la funcionalidad
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 export const Item = styled.li`
   padding: 0 8px;
-`
+`;
 
