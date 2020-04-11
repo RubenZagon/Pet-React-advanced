5fd112b557b3c2e0df2f5bdd57572d94f776b408
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-11 14:01:16 +0100

    🔥 Move GlobalStyles, and ✨ Added function animation FadeIn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {GlobalStyles} from './GlobalStyles';
+import {GlobalStyles} from './styles/GlobalStyles';
 import {ListOfCategories} from "./components/Molecules/ListOfCategories";
 import {ListOfPhotoCards} from "./components/Molecules/ListOfPhotoCards";
+import {Logo} from "./components/Atoms/Logo";
 
 export const App = () => (
   <div>
     <GlobalStyles/>
+    <Logo/>
     <ListOfCategories/>
     <ListOfPhotoCards/>
   </div>
--- src/components/Atoms/PhotoCard/styles.js
+++ src/components/Atoms/PhotoCard/styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import {fadeIn} from "../../../styles/animation";
+
 
 export const ImgWrapper = styled.div`
   border-radius: 10px;
@@ -10,7 +12,10 @@ export const ImgWrapper = styled.div`
   width: 100%;
 `;
 
+
 export const Img = styled.img`
+  // ${fadeIn({time: '200ms', type: 'easy-in'})}
+  ${fadeIn()};
   box-shadow: 0 10px 14px rgba(0,0,0, .2);
   height: 100%;
   // Para que la imagen se adapte al espacio que tiene
