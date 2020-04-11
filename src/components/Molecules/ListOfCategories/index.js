9e3d6b77136cefe4d27e4e3657d043929d569d68
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-11 17:15:21 +0100

    🎨 Addded effect when scroll to ListOfCategories

--- src/components/Molecules/ListOfCategories/index.js
+++ src/components/Molecules/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import {Category} from "../../Atoms/Category";
 import {Item, List} from "./styles";
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(function () {
     fetch('https://petgram-server-ruben.rubenzagon.now.sh/categories')
@@ -13,11 +14,31 @@ export const ListOfCategories = () => {
       })
   }, []);
 
-  return (
-    <List>
+  useEffect(function () {
+    const onScroll = event => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    };
+
+    // Esto lo necesitaremos para escuchar el documento, y que por cada vez que se haga un scroll, ejecute la función onScroll
+    document.addEventListener('scroll', onScroll);
+    // Esto es para que se limpie el efecto y no provoque un leak
+    return () => document.removeEventListener('scroll', onScroll)
+
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
       }
     </List>
+  );
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 };
--- src/components/Molecules/ListOfCategories/styles.js
+++ src/components/Molecules/ListOfCategories/styles.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components';
+import {fadeIn} from "../../../styles/animation";
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-  // Ocultar el scroll pero mantener la funcionalidad
-  &::-webkit-scrollbar {
-    display: none;
-  }
+   &.fixed {
+      ${fadeIn({time: '300ms'})};
+      background-color: #fff;
+      border-radius: 60px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+      left: 0;
+      margin: 0 auto;
+      max-width: 400px;
+      padding: 5px;
+      position: fixed;
+      right: 0;
+      top: -20px;
+      transform: scale(.5);
+      z-index: 1;
+    }
 `;
 
 export const Item = styled.li`
