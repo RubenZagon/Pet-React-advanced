378d7294881de2544da1eefe8a52c0952b3b5247
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-11 17:21:21 +0100

    Use props on ListOfCategories, to change the effect

--- src/components/Molecules/ListOfCategories/styles.js
+++ src/components/Molecules/ListOfCategories/styles.js
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {fadeIn} from "../../../styles/animation";
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-   &.fixed {
-      ${fadeIn({time: '300ms'})};
+  
+  ${props => props.fixed && css`
+    ${fadeIn({time: '300ms'})};
       background-color: #fff;
       border-radius: 60px;
       box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
@@ -19,7 +20,7 @@ export const List = styled.ul`
       top: -20px;
       transform: scale(.5);
       z-index: 1;
-    }
+ `}
 `;
 
 export const Item = styled.li`
