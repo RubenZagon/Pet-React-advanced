b86f69e4c759f4540c926760bc99f272b2c3635e
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-12 13:04:47 +0100

    🎨 Change the style of Categories

--- src/components/Atoms/Category/styles.js
+++ src/components/Atoms/Category/styles.js
@@ -1,20 +1,39 @@
 import styled from "styled-components";
 
 export const Anchor = styled.a`
-display: flex;
-flex-direction: column;
-text-align: center;
-text-decoration: none;
-width: 75px;
-
-`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  text-decoration: none;
+  width: 75px;
+`;
 
 export const Image = styled.img`
-border: 1px solid #ddd;
-box-shadow: 0px 10px 14px rgba(0,0,0,.2);
-border-radius: 50%;
-overflow: hidden;
-object-fit: cover;
-height: 75px;
-width: 75px;
-`
+  border: 3px solid #ddd;
+  border-radius: 50%;
+  overflow: hidden;
+  object-fit: cover;
+  height: 75px;
+  width: 75px;
+`;
+
+export const ColorBorder = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  background: rgb(252,204,99);
+  background: linear-gradient(48deg, 
+  rgba(252,204,99,1) 0%, 
+  rgba(251,173,80,1) 20%, 
+  rgba(188,42,141,1) 80%, 
+  rgba(138,58,185,1) 100%); 
+  
+  border-radius: 50%;
+  overflow: hidden;
+  height: 83px;
+  width: 83px;
+`;
+
+
+
