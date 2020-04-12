8c30c4f4e241c72a6bf3287c70e3751f0044a403
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-12 16:00:50 +0100

    Added compatibility with other navigatos - IntersectionObserver

--- src/customHooks/useLazyLoad.js
+++ src/customHooks/useLazyLoad.js
@@ -5,16 +5,22 @@ export function useLazyLoad() {
   const [show, setShow] = useState(false)
 
   useEffect(function () {
-    // Esto es para saber si ahora está en el view port el artículo Photo
-    const observer = new window.IntersectionObserver(function (entries) {
-      const {isIntersecting} = entries [0];
-      if (isIntersecting) {
-        setShow(true);
-        observer.disconnect();
-      }
-    });
-    observer.observe(element.current)
-  }, [element]);
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      // Esto es para saber si ahora está en el view port el artículo Photo
+      const observer = new window.IntersectionObserver(function (entries) {
+        const {isIntersecting} = entries [0];
+        if (isIntersecting) {
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      observer.observe(element.current)
+    }, [element]);
+  });
 
   return {element, show};
 }
