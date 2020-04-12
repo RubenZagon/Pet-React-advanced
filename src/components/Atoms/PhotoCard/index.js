fb294a2747dd3dd41109e613a9649ce3841288a5
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-12 15:09:41 +0100

    Added LazyLoad on photoCards

--- src/components/Atoms/PhotoCard/index.js
+++ src/components/Atoms/PhotoCard/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import {Button, Img, ImgWrapper} from "./styles";
+import React, {Fragment, useEffect, useRef, useState} from 'react';
+import {Article, Button, Img, ImgWrapper} from "./styles";
 import {MdFavoriteBorder} from "react-icons/md";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
+  const element = useRef(null);
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    // Esto es para saber si ahora está en el view port el artículo Photo
+    const observer = new window.IntersectionObserver(function (entries) {
+      const {isIntersecting} = entries [0];
+      if (isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(element.current)
+  }, [element]);
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt=""/>
-        </ImgWrapper>
-        <Button>
-          <MdFavoriteBorder size={'32px'}/> {likes} likes!
-        </Button>
-      </a>
-    </article>
+    <Article ref={element}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt=""/>
+            </ImgWrapper>
+            <Button>
+              <MdFavoriteBorder size={'32px'}/> {likes} likes!
+            </Button>
+          </a>
+        </Fragment>
+      }
+    </Article>
   )
 }
--- src/components/Atoms/PhotoCard/styles.js
+++ src/components/Atoms/PhotoCard/styles.js
@@ -3,6 +3,11 @@ import {fadeIn} from "../../../styles/animation";
 
 
 // noinspection JSUnresolvedFunction
+export const Article = styled.article`
+  min-height: 200px;
+`;
+
+
 export const ImgWrapper = styled.div`
   border-radius: 10px;
   display: block;
