79df3ea0e485fbcafd637649b1423518a4c4322e
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-15 10:42:27 +0100

    Implement GraphQL and reac-apollo in ListOfPhotoCards

--- src/components/Atoms/PhotoCard/index.js
+++ src/components/Atoms/PhotoCard/index.js
@@ -6,13 +6,10 @@ import {useLocalStorage} from "../../../hooks/useLocalStorage";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
-
   const {element, show} = useLazyLoad();
   const key = `like - ${id}`;
   const [liked, setLiked] = useLocalStorage(key, false);
-
   const Icon = liked ? MdFavorite : MdFavoriteBorder;
-
 
   return (
     <Article ref={element}>
