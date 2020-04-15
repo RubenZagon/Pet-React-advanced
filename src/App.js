5720c853cb18b936651096c42b0b5632fafd8968
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-15 11:02:06 +0100

    Used DetailID to render a 1 photo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import {ListOfCategories} from "./components/Molecules/ListOfCategories";
 import {ListOfPhotoCards} from "./components/Molecules/ListOfPhotoCards";
 import {Logo} from "./components/Atoms/Logo";
 
-export const App = () => (
-  <div>
-    <GlobalStyles/>
-    <Logo/>
-    <ListOfCategories/>
-    <ListOfPhotoCards/>
-  </div>
-);
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+  return (
+    <div>
+      <GlobalStyles/>
+      <Logo/>
+      {
+        detailId
+          ? <h1>Estamos usando detail ID</h1>
+          : <>
+            <ListOfCategories/>
+            <ListOfPhotoCards categoryId={1}/>
+          </>
+      }
+    </div>
+  )
+};
--- src/components/Molecules/ListOfPhotoCards/index.js
+++ src/components/Molecules/ListOfPhotoCards/index.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import {PhotoCard} from "../../Atoms/PhotoCard";
-
-import {graphql} from "react-apollo";
-import {gql} from 'apollo-boost'
-
-const withPhotos = graphql(gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`);
+import {withPhotos} from "../../../hoc/withPhotos";
 
 const ListOfPhotoCardsComponent = ({data: {photos = []}} = {}) => {
   return (
