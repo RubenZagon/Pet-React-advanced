1a53c6ecdb6a5e5a0b0f3ebae1dee7231a6d8250
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-15 11:51:07 +0100

    Now render the correct photo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {GlobalStyles} from './styles/GlobalStyles';
 import {ListOfCategories} from "./components/Molecules/ListOfCategories";
 import {ListOfPhotoCards} from "./components/Molecules/ListOfPhotoCards";
 import {Logo} from "./components/Atoms/Logo";
+import {PhotoCardWithQuery} from "./container/PhotoCardWithQuery";
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
@@ -14,11 +15,11 @@ export const App = () => {
       <Logo/>
       {
         detailId
-          ? <h1>Estamos usando detail ID</h1>
-          : <>
+          ? <PhotoCardWithQuery id={detailId}/>
+          : <Fragment>
             <ListOfCategories/>
             <ListOfPhotoCards categoryId={1}/>
-          </>
+          </Fragment>
       }
     </div>
   )
