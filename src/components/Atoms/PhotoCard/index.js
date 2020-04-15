86cc93bc102f574e6852ca9545dc2d4a65f3e075
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-15 15:28:19 +0100

    Extract FavButton

--- src/components/Atoms/PhotoCard/index.js
+++ src/components/Atoms/PhotoCard/index.js
@@ -1,15 +1,19 @@
 import React, {Fragment} from 'react';
-import {Article, Button, Img, ImgWrapper} from "./styles";
-import {MdFavorite, MdFavoriteBorder} from "react-icons/md";
+import {Article, Img, ImgWrapper} from "./styles";
 import {useLazyLoad} from "../../../hooks/useLazyLoad";
 import {useLocalStorage} from "../../../hooks/useLocalStorage";
+import {FavButton} from "../FavButton";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
+
+
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
   const {element, show} = useLazyLoad();
   const key = `like - ${id}`;
   const [liked, setLiked] = useLocalStorage(key, false);
-  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
+
+  const handleFavClick = () => setLiked(!liked);
 
   return (
     <Article ref={element}>
@@ -21,9 +25,10 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
             </ImgWrapper>
           </a>
 
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size={'32px'}/> {likes} likes!
-          </Button>
+          <FavButton liked={liked}
+                     likes={likes}
+                     onClick={handleFavClick}
+          />
         </Fragment>
       }
     </Article>
