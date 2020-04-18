4fb67f72378a7a7cb6e3b71d7d47efff6cc2ae41
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-18 14:59:20 +0100

    🎨 Change the style of navbar, and add current link

--- src/components/Atoms/NavBar/styles.js
+++ src/components/Atoms/NavBar/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {Link as LinkRouter} from "@reach/router";
+import {fadeIn} from "../../../styles/animation";
 
 export const Nav = styled.nav`
   align-items: center;
@@ -26,4 +27,17 @@ export const Link = styled(LinkRouter)`
   justify-content: center;
   align-items: center;
   text-decoration: none;
+  
+  &[aria-current] {
+    color: black;
+    
+    &:after {
+      ${fadeIn({time: '0.5s'})};
+      content: '·';
+      position: absolute;
+      bottom: 0;
+      font-size: 34px;
+      line-height: 20px;
+    }
+  }
 `;
