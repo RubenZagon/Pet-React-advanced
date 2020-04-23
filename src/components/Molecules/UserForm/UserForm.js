459f09ef736d721dd88e1d6363525110140feade
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-23 08:09:11 +0100

    Error when register a user already exist

--- src/components/Molecules/UserForm/UserForm.js
+++ src/components/Molecules/UserForm/UserForm.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import {useInputValue} from "../../../hooks/useInputValue";
-import {Button, Form, Input, Title} from "./styles";
+import {Button, Error, Form, Input, Title} from "./styles";
 
-export const UserForm = ({onSubmit, title}) => {
+export const UserForm = ({error, onSubmit, title}) => {
   const email = useInputValue('');
   const password = useInputValue('');
 
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     onSubmit({email: email.value, password: password.value})
   };
 
   return (
-    // <form onSubmit={onSubmit}>
-    //   <input placeholder={'Email'} value={email.value} onChange={email.onChange}/>
-    //   <input placeholder={'Password'} type={'password'} value={password.value} onChange={password.onChange}/>
-    //   <button>Iniciar sesión</button>
-    // </form>
     <>
-      <Title>{title}</Title>
       <Form onSubmit={handleSubmit}>
+        <Title>{title}</Title>
         <Input placeholder={'Email'} {...email}/>
         <Input placeholder={'Password'} type={'password'} {...password}/>
         <Button>{title}</Button>
       </Form>
+      {error && <Error>{error}</Error>}
     </>
   )
 };
