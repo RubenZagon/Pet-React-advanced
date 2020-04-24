aed6ba1c6920eefc9a88f2c3af0d08c8a5d3f7c0
Rubén Zagon <ruben.zagon@gmail.com>
2020-04-24 01:14:25 +0100

    Donwgrade dependencies of apollo and take the code to Apollo client

--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost'
-import {ApolloProvider} from 'react-apollo'
-import {App} from "./App";
-import Context from "./Context";
 
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'https://petgram-server-ruben.now.sh/graphql'
-});
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
+
+import Context from './Context'
+
+import {App} from './App'
 
 const client = new ApolloClient({
-  cache,
-  link,
-  request: (operation) => {
+  uri: 'https://petgram-server-ruben.now.sh/graphql',
+  request: operation => {
     const token = window.sessionStorage.getItem('token');
+    const authorization = token ? `Bearer ${token}` : '';
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : ''
+        authorization
       }
     })
-  },
-  onError: error => {
-    const {networkError} = error;
-    if (networkError && networkError.result.code === 'invalid_token') {
-      window.sessionStorage.removeItem('token');
-      window.location.href = '/';
-    }
   }
 });
+
 
 ReactDOM.render(
   <Context.Provider>
@@ -38,3 +30,5 @@ ReactDOM.render(
   </Context.Provider>,
   document.getElementById('app')
 );
+
+
