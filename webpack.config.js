6fd38782d65ca0b671f3e202a02a3b965b7f1d94
Rubén Zagon <ruben.zagon@gmail.com>
2020-05-05 10:41:23 +0100

    Change name manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+const path = require('path');
 
 module.exports = {
   output: {
@@ -8,6 +12,43 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      filename: 'manifest.webmanifest',
+      name: 'Petgram - Tu app de fotos de mascotas',
+      short_name: 'Petgram 😺',
+      description: 'Con Petgram puedes encontrar fotos de animales domésticos muy fácilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('Icons'),
+          ios: true,
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          // Dirección desde donde está sacando las imagenes
+          urlPattern: new RegExp('https://(res.cloudinary.com | images.unplash.com)'),
+          // Primero mirará si el recurso está en al caché, antes que en la red
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          // Dirección desde donde está desplegado el servidor
+          urlPattern: new RegExp('https://petgram-server-ruben.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
