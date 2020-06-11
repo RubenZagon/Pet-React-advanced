63f2d7b7acc8745ee3789485672d341ea8f301fa
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-11 18:39:51 +0100

    Add test on cypress and configuration

--- cypress/integration/petgram/petgram.spec.js
+++ cypress/integration/petgram/petgram.spec.js
@@ -1,5 +1,22 @@
 describe('Petgram', function () {
-  it('para ver si la app funciona', function () {
+  it('show if the app works', function () {
     cy.visit('/');
+  });
+
+  it('go to category and show a single photo', function () {
+    cy.visit('/pet/1');
+    cy.get('article');
+  });
+
+  it('go to home page using the navbar', function () {
+    cy.visit('/pet/1');
+    cy.get('nav a').first().click();
+    cy.url().should('include', '/');
+  });
+
+  it('if a not register or login user go to favs, must show the forms to access ', function () {
+    cy.visit('/favs');
+    cy.get('form').should('have.length', 2);
   })
+
 });
